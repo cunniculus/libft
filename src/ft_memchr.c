253859b831b1e04ef533baa355721bdffb3021a3
guolivei <guolivei@student.42sp.org.br>
2022-09-14 20:24:08 +0200

    new ft_memchar.c

--- src/ft_memchr.c
+++ src/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: guolivei <guolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 21:19:51 by guolivei          #+#    #+#             */
-/*   Updated: 2022/09/14 20:01:26 by guolivei         ###   ########.fr       */
+/*   Updated: 2022/09/14 20:22:48 by guolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	i;
+	void	*cpy_s;
 
+	cpy_s = (void*) s;
 	i = 0;
 	while (i < n)
 	{
 		if (((unsigned char *)s)[i] == (unsigned char)c)
-			return ((void *) (&s[i]));
+			return (&cpy_s[i]);
 		i++;
 	}
 	return (NULL);
