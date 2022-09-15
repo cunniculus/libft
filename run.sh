7fd1306c31aaa9be52e11c56589c87b5abccda2b
guolivei <guolivei@student.42sp.org.br>
2022-09-15 13:52:47 +0200

    updating run.sh to run on my directory

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 GREEN='\033[0;32m'
 NO_COLOR='\033[0m' # No Color
 
-cd tests && \
+cd tests/tests_from_scratch && \
 make && \
 make leakfull && \
-cd ../src && \
+cd ../../src && \
 norminette && \
 echo ""  && \
 echo -e "${GREEN}SUCESSO!${NO_COLOR}" 
